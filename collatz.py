f1d66bc5e3c56f732788afddf6cfd57134b2890a
Bryan Pearson <peabryan95@gmail.com>
2020-12-21 12:29:31 -0500

    Added the option to terminate when n is a power of 2

--- collatz.py
+++ collatz.py
@@ -1,19 +1,29 @@
 import matplotlib.pyplot as plt
+import math
 
-runs = 1000000 # Change this to whatever you want
+runs = 100 # The largest n value to be considered
+stop_at_power_of_two = True # If true, termiminate the Collatz sequence when n is a power of 2.
 
 lastSum = 0
 results = []
 averages = []
 maxes = []
 
-def collatz(n):
-    if n == 1:
+# Return True if n is a power of 2 (including 1)
+def isPowerOfTwo(n):
+    p = math.log(n, 2)
+    return math.floor(p) == math.ceil(p)
+
+# Given n, return the length of the Collatz sequence beginning at f(n)
+# If stop_at_power_of_two is True, then we stop when n is a power of 2
+# Otherwise, we stop when n is 1
+def collatz(n, stop_at_power_of_two):
+    if n == 1 or (stop_at_power_of_two and isPowerOfTwo(n)):
         return 1
     if n % 2 == 0:
-        return 1 + collatz(n / 2)
+        return 1 + collatz(n / 2, stop_at_power_of_two)
     else:
-        return 1 + collatz(n * 3 + 1)
+        return 1 + collatz(n * 3 + 1, stop_at_power_of_two)
 
 def findAverage(lastSum, nextElement, numElements):
     return (lastSum + nextElement, (lastSum + nextElement) / numElements)
@@ -24,7 +34,7 @@ def findMax(lastMax, candidate):
     return lastMax
 
 for s in range(1, runs + 1):
-    c = collatz(s)
+    c = collatz(s, stop_at_power_of_two)
     results.append(c)
     
     lastSum, average = findAverage(lastSum, c, len(results))
